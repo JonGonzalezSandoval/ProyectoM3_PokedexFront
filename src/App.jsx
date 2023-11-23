ec5ef2d06ab1daadb012bba2599f6242db7464ca
JonGonzalezSandoval <jongonzalezsandoval@gmail.com>
2023-11-23 15:21:08 +0100

    working on dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [listaPokemon, setListaPokemon] = useState(null)
+
+  function pedidaTest(){
+    fetch('http://localhost:3000/api/pokemon/paginated')
+  .then(res => res.json())
+  .then(res => console.log(res))
+}
+
+useEffect(() => {
+  pedidaTest()
+},[])
 
   return (
     <>
@@ -18,11 +28,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {}
         </p>
       </div>
       <p className="read-the-docs">
